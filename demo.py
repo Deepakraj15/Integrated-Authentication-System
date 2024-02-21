e4c46eb7f0a174d7857088765aab57afeab47676
Deepakraj15 <deepakraj15112002@gmail.com>
2024-02-21 20:59:57 +0530

    updated git ignore file

--- demo.py
+++ demo.py
@@ -2,7 +2,6 @@ import face_recognition
 image = face_recognition.load_image_file("Deepakraj_K.jpeg")
 capture = face_recognition.load_image_file("./CapturedImage/Benjamin.jpg")
 encoding1 = face_recognition.face_encodings(image)[0]
-print(encoding1)
 encoding2 = face_recognition.face_encodings(capture)[0]
 results = face_recognition.compare_faces([encoding1],encoding2)
 print(results)
