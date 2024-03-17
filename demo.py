64143d4f16c90d6d4cf8f09841b1c3415c0bf996
Deepakraj15 <deepakraj15112002@gmail.com>
2024-03-17 18:31:27 +0530

    repository cleaning

--- demo.py
+++
@@ -1,7 +0,0 @@
-import face_recognition
-image = face_recognition.load_image_file("Deepakraj_K.jpeg")
-capture = face_recognition.load_image_file("./CapturedImage/Benjamin.jpg")
-encoding1 = face_recognition.face_encodings(image)[0]
-encoding2 = face_recognition.face_encodings(capture)[0]
-results = face_recognition.compare_faces([encoding1],encoding2)
-print(results)
--- test.py
+++
@@ -1,8 +0,0 @@
-array = [[5, 3, 4],
-         [1, 5, 8],
-         [6, 4, 2]]
-
-for i in range(3):
-    for j in range(3):
-        if(i == j):
-            print(array[i][j],end=" ")
